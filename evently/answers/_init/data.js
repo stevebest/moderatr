6f3a686ba35a033ffc99ee91418ccbb7aa29d9e7
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2010-09-06 21:43:45 +0700

    Showing some additional info about answers

--- evently/answers/_init/data.js
+++ evently/answers/_init/data.js
@@ -1,14 +1,16 @@
 function (data) {
-  var answer;
+  $.log(data);
+
   var result = {
     answers : data.rows.map(function (row) {
-      answer = {};
+      var answer = {};
       answer.answer = row.value.answer;
-//      answer.created_at = $.prettyDate(row.value.created_at);
+      answer.created_at = row.key[1];
+      answer.pretty_created_at = $.prettyDate(answer.created_at);
+      answer.profile = row.value.profile;
       return answer;
     })
   };
-
   return result;
 }
 
