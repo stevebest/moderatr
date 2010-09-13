0655f1440b93f88139e8d3ecd15527a987c294eb
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2010-09-13 23:22:45 +0700

    Rewrite `questions/:id` into question _show request

--- shows/question.js
+++ shows/question.js
@@ -1,10 +1,13 @@
 function (doc) {
+  var ddoc = this;
   var Mustache = require('vendor/couchapp/lib/mustache');
   var Markdown = require('vendor/couchapp/lib/markdown');
-  // !json templates
+
   doc = doc || {};
   doc.question = Markdown.encode(doc.question || "");
 
-  return Mustache.to_html(templates.question, doc, templates.partials);
+  return Mustache.to_html(
+      ddoc.templates.question,
+      doc,
+      ddoc.templates.partials);
 }
-
