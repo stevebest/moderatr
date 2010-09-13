7cbf2c6546794e9521839434f8b9695bd5da4274
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2010-09-14 00:32:41 +0700

    Improve slug generation algorithm

--- evently/questionForm/loggedIn/selectors/textarea[name=question]/keyup.js
+++ evently/questionForm/loggedIn/selectors/textarea[name=question]/keyup.js
@@ -1,11 +1,29 @@
 function () {
-  // Translate question text into English and use it as a question ID.
   var text = this.value;
-  if (google && google.language && google.language.translate) {
-    google.language.translate(text, 'ru', 'en', function(result) {
-      if (result.translation) {
-        $('#questionSlugPreview').html(result.translation);
-      }
-    });
+  text = text || "";
+  text = text.toLowerCase().match(/^([^!?.]+)[!?.]?/);
+  text = text[0];
+
+  // Translate question text into English and use it as a question ID.
+  function generateSlug() {
+    if (google && google.language && google.language.translate) {
+      google.language.translate(text, 'ru', 'en', function(result) {
+        if (result.translation) {
+          var slug = result.translation.toLowerCase();
+          slug = slug.replace(/\W+/g, ' ');
+          slug = slug.replace(/^\s+/, '').replace(/\s+$/, '');
+          slug = slug.replace(/\s+/g, '-');
+          $('input[name=slug]').val(slug);
+        }
+      });
+    }
+  }
+
+  if (text != $$(this).text) {
+    $$(this).text = text;
+    if ($$(this).timeout) {
+      clearTimeout($$(this).timeout);
+    }
+    $$(this).timeout = setTimeout(generateSlug, 800);
   }
 }
